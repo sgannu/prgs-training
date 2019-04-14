97ef630033a3d5d91ca9cab0010ac99b93e4caf5
Srinivas Gannu <srinivas.gannu@gmail.com>
2019-04-14 17:15:13 -0500

    post method

--- calendar-java/src/main/java/com/prg/calendar/CalendarController.java
+++ calendar-java/src/main/java/com/prg/calendar/CalendarController.java
@@ -3,11 +3,15 @@ package com.prg.calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.prg.calendar.modal.Event;
 import com.prg.calendar.service.EventService;
 
 @Controller
@@ -34,5 +38,15 @@ public class CalendarController {
 	  return "calendar";
   }
 
+  @PostMapping(value="/createNewEvent", consumes={MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+  public String createNewEvent(@RequestBody Event event) {
+	  
+	  // here save the event 
+	  
+	return "eventSuccess"; //return view eventSuccess.html
+	  
+  }
+  
+  
 
 }
